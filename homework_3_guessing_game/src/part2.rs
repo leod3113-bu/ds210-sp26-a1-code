df34694bd5fb51007114ccc6d132d0775466c07d
DmmDGM <dm12332131mdgaming@gmail.com>
2026-02-12 09:28:56 -0500

    implemented part1, part2, and part3

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -7,6 +7,17 @@ pub struct Part2 {}
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        let mut a = min;
+        let mut b = max;
+        let mut m = (a + b) / 2;
+        while b - a > 1 {
+            match player.ask_to_compare(m) {
+                -1 => { b = m; }
+                1  => { a = m + 1; }
+                _  => { break; }
+            }
+            m = (a + b) / 2;
+        }
+        return m;
     }
 }
