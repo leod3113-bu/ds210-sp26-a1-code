8ce413821fc53b824dae96dd836869317d7d988b
Joselo2929 <josed29@bu.edu>
2026-03-22 15:31:23 -0400

    Student 2 work

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -1,4 +1,5 @@
 use kalosm::language::*;
+use rocket::tokio::fs;
 use crate::solution::file_library;
 
 pub struct ChatbotV4 {
@@ -17,7 +18,7 @@ impl ChatbotV4 {
 
         let mut chat_session: Chat<Llama> = self.model
             .chat()
-            .with_system_prompt("The assistant will act like a pirate");
+            .with_system_prompt("The assistant will act like luffy from one piece");
 
         // TODO: You have to implement the rest:
         // You need to load the chat session from the file using file_library::load_chat_session_from_file(...).
@@ -29,15 +30,26 @@ impl ChatbotV4 {
 
     pub fn get_history(&self, username: String) -> Vec<String> {
         let filename = &format!("{}.txt", username);
-
         match file_library::load_chat_session_from_file(&filename) {
+            // hello world
             None => {
                 return Vec::new();
             },
             Some(session) => {
-                // TODO: what should happen here?
-                return Vec::new();
+                // We want to return a vector of strings.
+                // We iterate through the whole vector
+                // and make every content within the vector a string
+                // and then collect it to make it a vector
+                return session
+                    .history()
+                    [1..]// so we dont take system prompt into consideration
+                    .iter()
+                    .map(|chat_message| String::from(chat_message.content()))
+                    .collect();
             }
         }
     }
+
+
 }
+
