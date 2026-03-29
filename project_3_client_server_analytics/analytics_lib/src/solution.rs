de69b1d5867738b422bcedcf6d96d3d7720302c2
leod3113-bu <leod3113@bu.edu>
2026-03-29 16:37:30 -0400

    implemented filter_dataset via recursive

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,8 +2,54 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+pub fn filter_parse(dataset: &Dataset, row: &Row, filter: &Condition) -> bool {
+    // Maps filter and parses them by operations
+    match filter {
+        Condition::Equal(column_name, expected_value) => filter_equal(dataset, row, column_name, expected_value),
+        Condition::Not(subfilter) => filter_not(dataset, row, subfilter),
+        Condition::And(subfilter_left, subfilter_right) => filter_and(dataset, row, subfilter_left, subfilter_right),
+        Condition::Or(subfilter_left, subfilter_right) => filter_or(dataset, row, subfilter_left, subfilter_right)
+    }
+}
+
+pub fn filter_equal(dataset: &Dataset, row: &Row, column_name: &String, expected_value: &Value) -> bool {
+    // Gets row value by fetching the column index from the dataset
+    let column_index = dataset.column_index(&column_name);
+    let row_value = row.get_value(column_index);
+
+    // Compares row value and expected value
+    *row_value == *expected_value
+}
+
+pub fn filter_not(dataset: &Dataset, row: &Row, filter: &Condition) -> bool {
+    // Parses condition into boolean, and then performs "not" on it
+    !filter_parse(dataset, row, filter)
+}
+
+pub fn filter_and(dataset: &Dataset, row: &Row, filter_left: &Condition, filter_right: &Condition) -> bool {
+    // Parses both conditions into booleans, and then performs "and" on them
+    filter_parse(dataset, row, filter_left) && filter_parse(dataset, row, filter_right)
+}
+
+pub fn filter_or(dataset: &Dataset, row: &Row, filter_left: &Condition, filter_right: &Condition) -> bool {
+    // Parses both conditions into booleans, and then performs "or" on them
+    filter_parse(dataset, row, filter_left) || filter_parse(dataset, row, filter_right)
+}
+
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    // Creates blank dataset (dataset columns are the same since we're only filtering)
+    let columns = dataset.columns();
+    let mut filtered_dataset = Dataset::new(columns.clone());
+
+    // Checks every row and appends to new dataset if passses
+    for row in dataset.iter() {
+        if filter_parse(dataset, row, filter) {
+            filtered_dataset.add_row(row.clone());
+        }
+    }
+
+    // Returns new dataset
+    return filtered_dataset;
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
