d3a6fd935e484e28e976445923c0acaed35f4d01
leod3113-bu <leod3113@bu.edu>
2026-04-11 19:13:12 -0400

    implemented flip and rotate board

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -1,37 +1,81 @@
 use std::collections::HashMap;
 
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::Board;
+use tic_tac_toe_stencil::board::{Board, Cell};
+use tic_tac_toe_stencil::layout::Layout;
 use tic_tac_toe_stencil::player::Player;
 
+use crate::layout::Layout3x3;
+
 // Your solution solution.
-pub struct SolutionAgent {
-    states: HashMap<String, Board>
+pub struct SolutionAgent {}
+
+// Pulls the board (or self if none) from the states hashmap
+pub fn get_shape(states: &HashMap<String, Board>, board: &Board) -> String {
+    todo!("smth");
 }
 
-impl SolutionAgent {
-    pub fn new() -> SolutionAgent {
-        return SolutionAgent { states: HashMap::new() };
+// Checks if the states hashmap has a similar shape of self via transposition
+pub fn has_shape(states: &HashMap<String, Board>, board: &Board) -> bool {
+    todo!("smth");
+}
+
+// Converts the board into a string notation
+pub fn hash_board(board: &Board) -> String {
+    todo!("smth");
+}
+
+// Rotates board clock-wise
+pub fn rotate_roard(board: &Board) -> Board {
+    let cells = board.get_cells();
+    let mut rotated = Board::new(Layout3x3 {});
+    for y in 0..cells.len() {
+        for x in 0..cells.len() {
+            let m = (y, x);
+            let m_prime = match m {
+                (0, 0) => (0, 2),
+                (0, 2) => (2, 2),
+                (2, 2) => (2, 0),
+                (2, 0) => (0, 0),
+                (0, 1) => (1, 2),
+                (1, 2) => (2, 1),
+                (2, 1) => (1, 0),
+                (1, 0) => (0, 1),
+                (1, 1) => (1, 1),
+                _ => panic!("Invalid cell")
+            };
+            match cell_to_player(cells, m) {
+                Some(player) => rotated.apply_move(m_prime, player),
+                None => ()
+            };
+        }
     }
+    rotated
+}
 
-    pub fn getState(board: &Board) -> Board {
-        
+// Flips board horizontally
+pub fn flip_board(board: &Board) -> Board {
+    let cells = board.get_cells();
+    let mut flipped = Board::new(Layout3x3 {});
+    for y in 0..cells.len() {
+        for x in 0..cells.len() {
+            let m = (y, x);
+            let m_prime = (y, 2 - x);
+            match cell_to_player(cells, m) {
+                Some(player) => flipped.apply_move(m_prime, player),
+                None => ()
+            }
+        }
     }
+    flipped
+}
 
-    pub fn getShape(board: &Board) -> String {
-
-    }
-
-    pub fn hashBoard(board: &Board) -> String {
-
-    }
-
-    pub fn rotateBoard(board: &Board) -> Board {
-
-    }
-
-    pub fn flipBoard(board: &Board) -> Board {
-
+// Turns Cell enum to Player enum at cell location
+pub fn cell_to_player(cells: &Vec<Vec<Cell>>, m: (usize, usize)) -> Option<Player> {
+    match cells[m.0][m.1] {
+        Cell::X => Some(Player::X),
+        Cell::O => Some(Player::O),
+        _ => None
     }
 }
 
