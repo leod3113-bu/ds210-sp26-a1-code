8bd6b3b644b04215950bdbca3a949a6eb1224eb4
Joselo2929 <josed29@bu.edu>
2026-04-19 17:45:19 -0400

    Implemented out own strucutres

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -1,6 +1,10 @@
+use std::arch::aarch64::float32x2_t;
+use std::collections::{HashMap, HashSet};
+
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::Board;
+use tic_tac_toe_stencil::board::{self, Board, Cell};
 use tic_tac_toe_stencil::player::Player;
+
 
 // Your solution solution.
 pub struct SolutionAgent {}
@@ -13,4 +17,44 @@ impl Agent for SolutionAgent {
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
         unimplemented!("Not yet implemented")
     }
+}
+
+
+pub struct OurBoard{
+    height: usize,
+    with: usize,
+    size: usize,
+    center: (usize,usize),
+    indicesx: HashSet<usize>,
+    indiceso: HashSet<usize>,
+    availables: HashSet<usize>,
+    transpositions : HashMap<String,(usize,i32)>,
+    priorities :HashMap<String, usize>,
+    
+    oneshot: bool, //so we are within the matrtix
+    cells: Vec<OurCell>,
+    timeout: usize,
+    since: usize,
+
+    player: Player,
+
+    straks: (usize,usize),
+    progression: f32,
+    winner: Option<Player>,
+    gameover: bool, 
+}
+
+pub struct OurCell {
+    board: OurBoard,
+    index: usize,
+    point: (usize,usize),
+    neighbours: (
+
+        Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>,
+        Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>,
+        Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>,
+        Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>, Option<Box<OurCell>>,
+    ),
+    value: Cell,
+    streaks: (usize,usize),
 }
